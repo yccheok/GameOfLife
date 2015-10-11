87b54b59cf392c8a42c071ae414c93cdd276bbb3
Yan Cheng Cheok <yccheok@yahoo.com>
2015-10-11 16:26:35 +0800

    test cases for edge.

--- app/src/test/java/org/yccheok/gameoflife/BasicEngineTest.java
+++ app/src/test/java/org/yccheok/gameoflife/BasicEngineTest.java
@@ -330,21 +330,21 @@ public class BasicEngineTest {
 
         cells = new Cells(6, 6);
         /*
-         . . . . . .
+         X . . . . X
          . X X . . .
          . X X . . .
-         . . . X X .
-         . . . X X .
          . . . . . .
+         . . . . . .
+         X . . . . X
          */
+        cells.getCell(0, 0).setAlive(true);
+        cells.getCell(0, 5).setAlive(true);
         cells.getCell(1, 1).setAlive(true);
         cells.getCell(1, 2).setAlive(true);
         cells.getCell(2, 1).setAlive(true);
         cells.getCell(2, 2).setAlive(true);
-        cells.getCell(3, 3).setAlive(true);
-        cells.getCell(3, 4).setAlive(true);
-        cells.getCell(4, 3).setAlive(true);
-        cells.getCell(4, 4).setAlive(true);
+        cells.getCell(5, 0).setAlive(true);
+        cells.getCell(5, 5).setAlive(true);
 
         cells = engine.nextGen(cells);
 
@@ -353,10 +353,10 @@ public class BasicEngineTest {
         assertEquals(false, cells.getCell(0, 2).isAlive());
         assertEquals(false, cells.getCell(0, 3).isAlive());
         assertEquals(false, cells.getCell(0, 4).isAlive());
-        assertEquals(false, cells.getCell(0, 5).isAlive());
+        assertEquals(true, cells.getCell(0, 5).isAlive());
 
         assertEquals(false, cells.getCell(1, 0).isAlive());
-        assertEquals(true, cells.getCell(1, 1).isAlive());
+        assertEquals(false, cells.getCell(1, 1).isAlive());
         assertEquals(true, cells.getCell(1, 2).isAlive());
         assertEquals(false, cells.getCell(1, 3).isAlive());
         assertEquals(false, cells.getCell(1, 4).isAlive());
@@ -364,7 +364,7 @@ public class BasicEngineTest {
 
         assertEquals(false, cells.getCell(2, 0).isAlive());
         assertEquals(true, cells.getCell(2, 1).isAlive());
-        assertEquals(false, cells.getCell(2, 2).isAlive());
+        assertEquals(true, cells.getCell(2, 2).isAlive());
         assertEquals(false, cells.getCell(2, 3).isAlive());
         assertEquals(false, cells.getCell(2, 4).isAlive());
         assertEquals(false, cells.getCell(2, 5).isAlive());
@@ -373,22 +373,22 @@ public class BasicEngineTest {
         assertEquals(false, cells.getCell(3, 1).isAlive());
         assertEquals(false, cells.getCell(3, 2).isAlive());
         assertEquals(false, cells.getCell(3, 3).isAlive());
-        assertEquals(true, cells.getCell(3, 4).isAlive());
+        assertEquals(false, cells.getCell(3, 4).isAlive());
         assertEquals(false, cells.getCell(3, 5).isAlive());
 
         assertEquals(false, cells.getCell(4, 0).isAlive());
         assertEquals(false, cells.getCell(4, 1).isAlive());
         assertEquals(false, cells.getCell(4, 2).isAlive());
-        assertEquals(true, cells.getCell(4, 3).isAlive());
-        assertEquals(true, cells.getCell(4, 4).isAlive());
+        assertEquals(false, cells.getCell(4, 3).isAlive());
+        assertEquals(false, cells.getCell(4, 4).isAlive());
         assertEquals(false, cells.getCell(4, 5).isAlive());
 
-        assertEquals(false, cells.getCell(5, 0).isAlive());
+        assertEquals(true, cells.getCell(5, 0).isAlive());
         assertEquals(false, cells.getCell(5, 1).isAlive());
         assertEquals(false, cells.getCell(5, 2).isAlive());
         assertEquals(false, cells.getCell(5, 3).isAlive());
         assertEquals(false, cells.getCell(5, 4).isAlive());
-        assertEquals(false, cells.getCell(5, 5).isAlive());
+        assertEquals(true, cells.getCell(5, 5).isAlive());
 
         cells = new Cells(6, 6);
         /*
